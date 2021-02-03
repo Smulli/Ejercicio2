1e61f8bc049903d1b0941aace06eb9a24af23f36
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-03 01:04:34 +0100

    Ejercicio 2 UD4.

---
+++ src/VariableChar.java
@@ -0,0 +1,13 @@
+public class VariableChar {
+	public static void main (String args[]) {
+		int N = 3;
+		double A = 4.56;
+		char caracter = 'a';
+		
+		System.out.println(N+A); //resultado de la suma.
+		System.out.println(N-A); //resultado de la resta.
+		System.out.print((int)caracter); //saco el valor numérico de 'a'.
+	}
+	
+}
+
